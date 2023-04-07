bbfae46f867f60f9acfed5aded022dca1b6fec6a
ClovisSecurity <126100008+ClovisSecurity@users.noreply.github.com>
2023-04-07 10:18:46 -0600

    Create get_historical_whois.py

---
+++ get_historical_whois.py
@@ -0,0 +1,18 @@
+import requests
+from endpoint_reqs import endpoint_info
+import json
+
+base_url = "https://www.virustotal.com/api/v3/"
+object_name = "ip_addresses/"
+ip_to_analyze = "104.244.42.198"
+
+url = f"{base_url}{object_name}{ip_to_analyze}/historical_whois"
+
+headers = {
+    "accept": "application/json",
+    "x-apikey": endpoint_info["api_key"]
+}
+
+response = requests.get(url, headers=headers).json()
+
+print(json.dumps(response, indent=4))
